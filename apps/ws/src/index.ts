af8da129dd5a644be3e7466e0b88fb215248c602
shujanshaikh <shaikhshujan@gmail.com>
2025-01-24 16:24:38 +0530

    Websocket

--- apps/ws/src/index.ts
+++ apps/ws/src/index.ts
@@ -1,13 +1,14 @@
 import { WebSocketServer } from 'ws';
+import { User } from './user';
 
 const wss = new WebSocketServer({ port: 3001 });
 
 wss.on('connection', function connection(ws) {
+  console.log("yser connected")
+  let user = new User(ws);
   ws.on('error', console.error);
 
-  ws.on('message', function message(data) {
-    console.log('received: %s', data);
+  ws.on('close', () => {
+    user?.destroy();
   });
-
-  ws.send('something');
 });
